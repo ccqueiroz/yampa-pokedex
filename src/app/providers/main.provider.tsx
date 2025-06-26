7aa6a249ee9e8285b748b20cf6cbeb421521132e
Caio Queiroz <caio.queiroz@terceiro.rnp.br>
2025-06-26 00:57:35 -0300

    chore: add styles to main provider

--- src/app/providers/main.provider.tsx
+++ src/app/providers/main.provider.tsx
@@ -1,8 +1,13 @@
+import { cn } from "@/lib/utils";
 import "../../config/i18n/i18n.config";
 
 type MainProviderProps = {
   children: React.ReactNode;
 };
 export const MainProvider = ({ children }: MainProviderProps) => {
-  return <>{children}</>;
+  return (
+    <main className={cn("min-h-screen min-w-[100vw] bg-background")}>
+      {children}
+    </main>
+  );
 };
