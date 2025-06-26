a62a680eae71cf11379ebc91f77f26246f2a3353
Caio Queiroz <caio.queiroz@terceiro.rnp.br>
2025-06-26 00:56:45 -0300

    chore: add styles to section and add header component

--- src/app/main/main.tsx
+++ src/app/main/main.tsx
@@ -1,3 +1,11 @@
+import { Header } from "@/app/components/header/header.component";
+
 export const Main = () => {
-  return <div></div>;
+  return (
+    <section className="xl:h-screen hsm:h-full relative min-h-screen flex items-center justify-center py-20 overflow-hidden">
+      <div className="container max-auto px-4 relative z-10">
+        <Header />
+      </div>
+    </section>
+  );
 };
