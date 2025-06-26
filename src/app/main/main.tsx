33a3bcc68c09503e5bffe5822006672efb2c567a
Caio Queiroz <caio.queiroz@terceiro.rnp.br>
2025-06-26 01:23:24 -0300

    fix: fixing responsiveness main

--- src/app/main/main.tsx
+++ src/app/main/main.tsx
@@ -2,7 +2,7 @@ import { Header } from "@/app/components/header/header.component";
 
 export const Main = () => {
   return (
-    <section className="xl:h-screen hsm:h-full relative min-h-screen flex items-center justify-center py-20 overflow-hidden">
+    <section className="min-w-screen xl:h-screen hsm:h-full relative min-h-screen overflow-hidden">
       <div className="container max-auto px-4 relative z-10">
         <Header />
       </div>
