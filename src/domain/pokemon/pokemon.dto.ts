40a6606d91b445d8edc7990fd36f2858b50a4e6e
Caio Queiroz <caio.queiroz@terceiro.rnp.br>
2025-06-29 01:35:48 -0300

    chore: add types to Response Fetch Pokemon

--- src/domain/pokemon/pokemon.dto.ts
+++ src/domain/pokemon/pokemon.dto.ts
@@ -17,3 +17,57 @@ export type ResponseListPokemon = {
   previous: QueriesParamsToListPokemon | null;
   results: ListPokemon;
 };
+
+export type TypesPokemon = Array<{ color: string; type: string }>;
+
+export type AbilitiesPokemon = Array<{ slot: number; ability: string }>;
+
+export type StatusPokemon = {
+  hp: number;
+  attack: number;
+  defense: number;
+  "special-attack": number;
+  "special-defense": number;
+  speed: number;
+  total: number;
+};
+
+export type AbilitiesBase = {
+  ability: {
+    name: string;
+    url: string;
+  };
+  is_hidden: boolean;
+  slot: number;
+};
+
+export type StatsBase = {
+  base_stat: number;
+  stat: { name: string };
+};
+
+export type TypeBase = {
+  type: { name: string };
+};
+
+export type ResponsePokemonService = {
+  abilities: Array<AbilitiesBase>;
+  base_experience: number;
+  height: number;
+  id: number;
+  name: string;
+  stats: Array<StatsBase>;
+  types: Array<TypeBase>;
+  weight: number;
+};
+
+export type ResponsePokemon = {
+  statusPokemon: StatusPokemon;
+  types: TypesPokemon;
+  name: string;
+  nameFormated: string;
+  weight: number;
+  height: number;
+  id: number;
+  abilities: AbilitiesPokemon;
+};
