d86b79f0903bf6d757b6bf84439a7e4c701007c8
Caio Queiroz <caio.queiroz@terceiro.rnp.br>
2025-07-02 08:32:11 -0300

    feature: create Wrapper to Group components Suggestions

---
+++ src/app/components/inputSearch/fragments/PokemonSuggestions/fragments/WrapperGroupSuggestions/wrapperGroupSuggestions.component.tsx
@@ -0,0 +1,38 @@
+import {
+  CommandEmpty,
+  CommandGroup,
+  CommandList,
+} from "@/app/components/ui/command";
+import { useI18n } from "@/app/hooks/usei18n.hook";
+
+export const WrapperGroupSuggestions = ({
+  children,
+}: {
+  children: React.ReactNode;
+}) => {
+  const { translation } = useI18n();
+
+  return (
+    <div className="min-h-4 absolute top-0 z-10 w-full rounded-md border bg-popover text-popover-foreground shadow-md outline-none">
+      <CommandList>
+        <CommandEmpty className="p-0">
+          <div className="w-full flex p-1 gap-1">
+            <div className="pt-1 ml-2">
+              <picture>
+                <img
+                  src="src/assets/pokedex.png"
+                  className="w-12 h-8"
+                  alt={translation("accessibility.pokedex")}
+                />
+              </picture>
+            </div>
+            <span className="text-start text-sm text-primary text-wrap">
+              {translation("inputs.empty_pokemon_list_suggestion")}
+            </span>
+          </div>
+        </CommandEmpty>
+        <CommandGroup className="h-full overflow-auto">{children}</CommandGroup>
+      </CommandList>
+    </div>
+  );
+};
