61fd3100b3503e8182eaccf5188beecb2a9f670b
Caio Queiroz <caio.queiroz@terceiro.rnp.br>
2025-07-02 08:25:19 -0300

    feature: create InputCommand

---
+++ src/app/components/inputSearch/fragments/InputCommand/inputCommand.component.tsx
@@ -0,0 +1,69 @@
+import { observer } from "mobx-react-lite";
+import { Search, SendHorizontal } from "lucide-react";
+import { Command as CommandPrimitive } from "cmdk";
+import { useCallback } from "react";
+import { useI18n } from "@/app/hooks/usei18n.hook";
+import { Button } from "../../../ui/button";
+import { useInputSearch } from "../../context/InputSearch.provider";
+import { usePokemonsList } from "@/app/context/usePokemonsList.context";
+import { useDebounce } from "@/app/hooks/useDebouce.hook";
+import { cn } from "@/lib/utils";
+
+export const InputCommand = observer(() => {
+  const { translation } = useI18n();
+  const {
+    setOpenSugestions,
+    selectedSugestion,
+    setSelectedSugestion,
+    inputSearchPokemonRef,
+  } = useInputSearch();
+  const { setSugestions, getBySuggestedPokemons, clearSugestions } =
+    usePokemonsList();
+  const { debounce } = useDebounce();
+
+  const handleOnChange = useCallback(
+    async (search: string) => {
+      setSelectedSugestion(search);
+
+      if (search.length < 3) {
+        clearSugestions();
+        return;
+      }
+
+      debounce(() => setSugestions(search), 300);
+    },
+    [clearSugestions, debounce, setSelectedSugestion, setSugestions]
+  );
+
+  return (
+    <div className="w-full flex items-center rounded-md border border-border bg-background px-2">
+      <Search className="h-4 w-4 text-secondary mb-[2px]" aria-hidden="true" />
+      <CommandPrimitive.Input
+        ref={inputSearchPokemonRef}
+        name="input-search-pokemon"
+        value={selectedSugestion}
+        aria-label={translation("inputs.placeholder")}
+        placeholder={translation("inputs.placeholder")}
+        className={cn(
+          "w-full h-9 flex-1 outline-none placeholder:text-secondary pl-2 pr-1 bg-muted text-secondary text-base transition-colors",
+          "disabled:cursor-not-allowed disabled:opacity-50",
+          "md:text-sm"
+        )}
+        onValueChange={handleOnChange}
+        onBlur={() => setOpenSugestions(false)}
+        onFocus={() => setOpenSugestions(true)}
+      />
+      <Button
+        name="btn-search-pokemon"
+        variant="ghost"
+        size="icon"
+        className="h-6 w-6 text-secondary hover:bg-transparent"
+        aria-label={translation("inputs.placeholder")}
+        onClick={() => getBySuggestedPokemons(selectedSugestion)}
+        disabled={selectedSugestion.length < 3}
+      >
+        <SendHorizontal className="h-5 w-5" aria-hidden="true" />
+      </Button>
+    </div>
+  );
+});
