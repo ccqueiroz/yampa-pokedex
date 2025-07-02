57440ef5d86f9b714e55dba3ad188147fe784558
Caio Queiroz <caio.queiroz@terceiro.rnp.br>
2025-07-02 08:24:36 -0300

    refactor: refatored InputSearch to include Command components to autocomplete

--- src/app/components/inputSearch/inputSearch.stories.tsx
+++ src/app/components/inputSearch/inputSearch.stories.tsx
@@ -1,15 +1,27 @@
 import type { Meta, StoryObj } from "@storybook/react-vite";
 
 import { InputSearch } from "./inputSearch.component";
+import { pokemonListStore } from "@/infra/store/pokemonList.store";
 
 const meta = {
-  title: "Input Search",
+  title: "InputSearch",
   component: InputSearch,
 } satisfies Meta<typeof InputSearch>;
 
 export default meta;
+
 type Story = StoryObj<typeof meta>;
 
-export const Primary: Story = {
-  args: {},
+pokemonListStore.pokemonsSearchSugestion = [
+  { id: "1", name: "bulbasaur" },
+  { id: "4", name: "charmander" },
+  { id: "7", name: "squirtle" },
+];
+
+export const Default: Story = {
+  render: () => {
+    return <InputSearch />;
+  },
 };
+
+Default.storyName = "Primary";
